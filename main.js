d7f12746c86b22a8ca60b44f612043c3a8860cf1
agarioch <alistairgarioch@gmail.com>
2021-10-17 21:47:02 +0100

    Feat(FAQ): Add mobile layout, not responsive, JS not finished

---
+++ main.js
@@ -0,0 +1,13 @@
+const accordionItems = document.querySelectorAll('.faq__list__item');
+console.log(accordionItems);
+for (let item of accordionItems) {
+  item.addEventListener('click', toggleAccordion)
+}
+
+function toggleAccordion(e) {
+  const listItem = e.currentTarget;
+  const question = listItem.querySelector('.faq__list__question--text');
+  question.classList.toggle('faq__list__question--selected');
+  const answer = listItem.querySelector('.faq__list__answer');
+  answer.classList.toggle('faq__list__answer--hidden');
+}
