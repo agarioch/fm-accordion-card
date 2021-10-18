14a3e2ad3b17000a9b7a1faacd4d29a4289defcc
agarioch <alistairgarioch@gmail.com>
2021-10-18 18:09:04 +0100

    Feat(FAQ): Add desktop layout for FAQ card

--- main.js
+++ main.js
@@ -6,8 +6,13 @@ for (let item of accordionItems) {
 
 function toggleAccordion(e) {
   const listItem = e.currentTarget;
+
   const question = listItem.querySelector('.faq__list__question--text');
   question.classList.toggle('faq__list__question--selected');
+
   const answer = listItem.querySelector('.faq__list__answer');
   answer.classList.toggle('faq__list__answer--hidden');
+
+  const arrow = listItem.querySelector('.faq__list__question--icon');
+  arrow.classList.toggle('faq__list__question--up-icon');
 }
